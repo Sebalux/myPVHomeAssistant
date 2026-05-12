e8cb660c618691127e05f74866e5f370a87dd7fe
Sebalux <88102773+Sebalux@users.noreply.github.com>
2026-05-12 21:32:22 +0200

    Update number.py
    added working slider for maxpwr (Maximum Power Setting for AC-Thor (9S)) and fixed working slider to send data back to AC-Thor for ww1boost setting

--- custom_components/mypv/number.py
+++ custom_components/mypv/number.py
@@ -1,109 +1,107 @@
-from homeassistant.components.number import NumberEntity
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.const import UnitOfTemperature, CONF_HOST, CONF_DEVICE
-from homeassistant.helpers.event import async_track_time_interval
 import logging
-
-from .const import DOMAIN, DATA_COORDINATOR, DEFAULT_MAX_VALUE, DEFAULT_MIN_VALUE, DEFAULT_MODE, DEFAULT_STEP, WIFI_METER_NAME, MIN_TIME_BETWEEN_UPDATES
-from .coordinator import MYPVDataUpdateCoordinator
+import aiohttp
+from homeassistant.components.number import NumberEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .const import DOMAIN, DATA_COORDINATOR
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities):
-    """Set up the WWBoost number entity."""
-    device_name = entry.data[CONF_DEVICE]
-    if device_name != WIFI_METER_NAME:
-        coordinator: MYPVDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
-        host = entry.data[CONF_HOST]
-        async_add_entities([WWBoost(coordinator, host, entry.title)])
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Setup myPV numbers from a config entry."""
+    coordinator = hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
+    # Wir nutzen entry.title, genau wie in der sensor.py
+    device_name = entry.title
+    
+    async_add_entities([
+        MyPVMaxPwr(coordinator, device_name),
+        MyPVWWBoost(coordinator, device_name)
+    ])
 
-class WWBoost(CoordinatorEntity, NumberEntity):
-    """Representation of the WWBoost number entity"""
-
-    def __init__(self, coordinator, host, name):
-        """Initialize the number entity."""
+class MyPVNumberEntity(CoordinatorEntity, NumberEntity):
+    """Gemeinsame Basis für myPV Slider mit korrekter Device Info."""
+    def __init__(self, coordinator, device_name):
         super().__init__(coordinator)
-        self._device_name = name
-        self._host = host
-        self._min_value = DEFAULT_MIN_VALUE
-        self._max_value = DEFAULT_MAX_VALUE
-        self._value = float(self.coordinator.data.get("setup", {}).get("ww1boost", 500)) / 10
-        self._step = DEFAULT_STEP
-        self._unit_of_measurement = UnitOfTemperature.CELSIUS
-        self._mode = DEFAULT_MODE
-        self.serial_number = self.coordinator.data.get("info", {}).get("sn", "unknown")
-        self._model = self.coordinator.data.get("info", {}).get("device", "unknown")
-        self._number = f"ww1boost_{self._host}"
-        self._name = f"Hot Water Assurance {self._host}"
+        self._device_name = device_name
+        # Wir holen die Hardware-Infos exakt wie in der sensor.py
+        self.serial_number = self.coordinator.data["info"]["sn"]
+        self.model = self.coordinator.data["info"]["device"]
+        self._host = self.coordinator._host # Zugriff auf den Host im Coordinator
 
     @property
     def device_info(self):
-        """Return information about the device."""
+        """Verknüpft die Entität exakt mit dem Hauptgerät."""
         return {
             "identifiers": {(DOMAIN, self.serial_number)},
             "name": self._device_name,
             "manufacturer": "my-PV",
-            "model": self._model,
+            "model": self.model,
         }
-    
-    @property
-    def unique_id(self):
-        """Return unique id based on device serial and variable."""
-        return "{} {}".format(self.serial_number, self._number)
 
+class MyPVMaxPwr(MyPVNumberEntity):
+    """Slider für die maximale Leistung."""
     @property
     def name(self):
-        """Return the display name of this entity."""
-        return self._name
+        return f"{self._device_name} Maximum Power"
 
     @property
-    def native_min_value(self):
-        """Return the minimum value of this number."""
-        return self._min_value
-
-    @property
-    def native_max_value(self):
-        """Return the maximum value of this number."""
-        return self._max_value
+    def unique_id(self):
+        return f"{self.serial_number} Maximum Power"
 
     @property
     def native_value(self):
-        """Return the current value of this number."""
-        return self._value
-    
-    @property
-    def native_step(self):
-        """Return the step size for this number."""
-        return self._step
-    
+        val = self.coordinator.data.get("setup", {}).get("maxpwr")
+        return float(val) if val is not None else None
+
     @property
     def native_unit_of_measurement(self):
-        """Return the unit of measurement for this number."""
-        return self._unit_of_measurement
+        return "%"
+
+    @property
+    def native_min_value(self):
+        return 0
     
     @property
-    def mode(self):
-        """Return mode of this entity"""
-        return self._mode
+    def native_max_value(self):
+        return 100
 
-    async def async_added_to_hass(self):
-        """Handle entity which will be added to hass."""
-        await super().async_added_to_hass()
-        async_track_time_interval(self.hass, self._async_poll, MIN_TIME_BETWEEN_UPDATES)
+    async def async_set_native_value(self, value):
+        url = f"http://{self._host}/setup.jsn?maxpwr={int(value)}"
+        async with aiohttp.ClientSession() as session:
+            await session.get(url, timeout=5)
+            await self.coordinator.async_request_refresh()
 
-    async def _async_poll(self, now):
-        """Poll for updates."""
-        await self.coordinator.async_request_refresh()
-        if self.coordinator.last_update_success:
-            self._value = self.coordinator.data.get("setup", {}).get("ww1boost", 500) / 10
-            self.async_write_ha_state()
+class MyPVWWBoost(MyPVNumberEntity):
+    """Slider für die Warmwasser-Sicherstellung."""
+    @property
+    def name(self):
+        return f"{self._device_name} Hot Water Boost"
 
-    async def async_set_native_value(self, value: float):
-        """Set a new value for this number."""
-        if self._min_value <= value <= self._max_value:
-            self._value = value
-            await self.coordinator.async_refresh()
-            self.async_write_ha_state()
-        else:
-            _LOGGER.error(f"Value {value} is out of range [{self._min_value}, {self._max_value}]")
+    @property
+    def unique_id(self):
+        return f"{self.serial_number} Hot Water Boost"
+
+    @property
+    def native_value(self):
+        val = self.coordinator.data.get("setup", {}).get("ww1boost")
+        # Skalierung: Gerät liefert 500 für 50.0°C
+        return float(val) / 10.0 if val is not None else None
+
+    @property
+    def native_unit_of_measurement(self):
+        return "°C"
+
+    @property
+    def native_min_value(self):
+        return 20
+    
+    @property
+    def native_max_value(self):
+        return 90
+
+    async def async_set_native_value(self, value):
+        # Skalierung zurück: HA liefert 45.5 -> 455 für das Gerät
+        target_val = int(value * 10)
+        url = f"http://{self._host}/setup.jsn?ww1boost={target_val}"
+        async with aiohttp.ClientSession() as session:
+            await session.get(url, timeout=5)
+            await self.coordinator.async_request_refresh()
